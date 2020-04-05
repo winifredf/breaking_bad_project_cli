6336f28c6b04e51a250b15869b941964ae545dc1
Winifred Frierson <winifred.dykes@hotmail.com>
2020-04-05 01:55:50 +0000

    defined call_api method

--- lib/api.rb
+++ lib/api.rb
@@ -1,11 +1,13 @@
 class API
+  
+  def call_api
 
-resp = RestClient.get("https://www.breakingbadapi.com/api/characters")
-characters_hash = JSON.parse(resp.body, symbolize_names:true)
-characters_array = characters_hash
-
-characters_array.each do |char|
-  Character.new(char[:char_id], char[:name], char[:birthday], char[:occupation], char[:img], char[:status], char[:appearance], char[:nickname], char[:portrayed])
-end
-
+    resp = RestClient.get("https://www.breakingbadapi.com/api/characters")
+    characters_hash = JSON.parse(resp.body, symbolize_names:true)
+    characters_array = characters_hash
+    
+    characters_array.each do |char|
+      Character.new(char[:char_id], char[:name], char[:birthday], char[:occupation], char[:img], char[:status], char[:appearance], char[:nickname], char[:portrayed])
+    end
+  end
 end
