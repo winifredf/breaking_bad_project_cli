5e97ed6e6a856c475f9ecbfa07230d9dae3a4f5c
Winifred Frierson <winifred.dykes@hotmail.com>
2020-04-05 01:16:01 +0000

    created new characters

--- api.rb
+++ api.rb
@@ -4,5 +4,10 @@ require 'json'
 
 resp = RestClient.get("https://www.breakingbadapi.com/api/characters")
 characters_hash = JSON.parse(resp.body, symbolize_names:true)
-characters_array = characters_hash[0]
+characters_array = characters_hash
+
+characters_array.each do |char|
+  binding.pry
+  Character.new(char_id, name, birthday, occupation, img, status, appearance, nickname, portrayed)
+end
 
