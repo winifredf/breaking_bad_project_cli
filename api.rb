42c8659e2ca00eb607c923dd9e69becaffbe29f7
Winifred Frierson <winifred.dykes@hotmail.com>
2020-04-05 01:24:39 +0000

    require_relative characters.rb

--- api.rb
+++ api.rb
@@ -1,13 +1,13 @@
 require 'pry'
 require 'rest-client'
 require 'json'
+require_relative 'characters.rb'
 
 resp = RestClient.get("https://www.breakingbadapi.com/api/characters")
 characters_hash = JSON.parse(resp.body, symbolize_names:true)
 characters_array = characters_hash
 
 characters_array.each do |char|
-  binding.pry
-  Character.new(char_id, name, birthday, occupation, img, status, appearance, nickname, portrayed)
+  Character.new(char[:char_id], char[:name], char[:birthday], char[:occupation], char[:img], char[:status], char[:appearance], char[:nickname], char[:portrayed])
 end
 
