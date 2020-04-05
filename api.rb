a537c7dd4139c092803621705b395f3edddcac71
Winifred Frierson <winifred.dykes@hotmail.com>
2020-04-05 00:23:23 +0000

    character's exit

--- api.rb
+++ api.rb
@@ -4,3 +4,5 @@ require 'json'
 
 resp = RestClient.get("https://www.breakingbadapi.com/api/characters")
 characters_hash = JSON.parse(resp.body, symbolize_names:true)
+characters_array = characters_hash[0]
+binding.pry
