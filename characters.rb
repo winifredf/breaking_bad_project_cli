fe72d3fb0e581f525b7b54a510e016b380884aa6
Winifred Frierson <winifred.dykes@hotmail.com>
2020-04-05 01:07:50 +0000

    def method

--- characters.rb
+++ characters.rb
@@ -1,5 +1,23 @@
 class Character
-  def initialize
-    
+  
+  attr_accessor :char_id, :name, :birthday, :occupation, :img, :status, :appearance, :nickname, :portrayed
+  
+  @@all = []
+  
+  def initialize(char_id, name, birthday, occupation, img, status, appearance, nickname, portrayed)
+    @char_id = char_id
+    @name = name
+    @birthday = birthday
+    @occupation = occupation
+    @img = img
+    @status = status
+    @appearance = appearance
+    @nickname = nickname
+    @portrayed = portrayed
+    @@all << self
+  end
+  
+  def self.all
+    @@all
   end
 end
